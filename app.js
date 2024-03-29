1dc6ecc23d92ab0543f9d925b7b4a7439d15ae15
SARTHAK KHARE <113530791+the-code-machine@users.noreply.github.com>
2024-03-29 15:23:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const port= process.env.PORT|| 4000;
 const cors = require('cors');
 const mongoose= require('mongoose');
 const config = require('./DB');
-app.use(cors({
-  origin: 'http://127.0.0.1:5500' // Replace this with your frontend URL
-}));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
